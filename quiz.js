c5cc3155777bc424001f14f15682328c9f8e7e6e
Abhaya Parida <115058980+Abhaya444@users.noreply.github.com>
2024-01-18 07:14:03 +0530

    completed

--- quiz.js
+++ quiz.js
@@ -1,59 +1,120 @@
-const questionObj = 
-    {
-      category: 'Food & Drink',
-      id: 'qa-1',
-      correctAnswer: 'Three',
-      options: ['Two', 'Three ', 'Four', 'Five'],
-      question:
-        "How many pieces of bun are in a Mcdonald's Big Mac?",
-  };
-
-const {correctAnswer, options, question} = questionObj;
+const quesJSON = [
+  {
+    correctAnswer: "Three ",
+    options: ["Two", "Three ", "Four", "Five"],
+    question: "How many pieces of bun are in a Mcdonald's Big Mac?",
+  },
+  {
+    correctAnswer: "L. Frank Baum",
+    options: [
+      "Suzanne Collins",
+      "James Fenimore Cooper",
+      "L. Frank Baum",
+      "Donna Leon",
+    ],
+    question: "Which author wrote 'The Wonderful Wizard of Oz'?",
+  },
+  {
+    correctAnswer: "Atlanta United",
+    options: [
+      "Atlanta United",
+      "Atlanta Impact",
+      "Atlanta Bulls",
+      "Atlanta Stars",
+    ],
+    question: "Which of these is a soccer team based in Atlanta?",
+  },
+  {
+    correctAnswer: "A Nanny",
+    options: ["A Sow", "A Lioness", "A Hen", "A Nanny"],
+    question: "A female goat is known as what?",
+  },
+  {
+    correctAnswer: "P. L. Travers",
+    options: [
+      "J. R. R. Tolkien",
+      "P. L. Travers",
+      "Lewis Carroll",
+      "Enid Blyton",
+    ],
+    question: "Which author wrote 'Mary Poppins'?",
+  },
+];
 
 let score = 0;
 let currentQuestion = 0;
+let totalScore = quesJSON.length;
 
 const questionElement = document.getElementById("question");
-questionElement.textContent = questionObj.question;
 
 //To fetch option div
 const optionEl = document.getElementById("options");
-//To fetch score 
+//To fetch score
 const scoreEl = document.getElementById("score");
 
-//Populating the Options div with the buttons
-options.forEach((opt) => {
-  // Creating button  and append it to the option div
-  const btn = document.createElement('button');
-  btn.textContent = opt;
-  optionEl.appendChild(btn);
+const nextEl = document.getElementById("next");
 
-  //Event handling on the button
-  btn.addEventListener("click", ()=> {
-    if(opt === correctAnswer){
-      score++;
-    }else{
-      score = score - 0.25;
-    }
-    console.log(score);
-    scoreEl.textContent = `Score: ${score}`;
-    
-  })
+showQuestion();
+
+nextEl.addEventListener('click', ()=>{
+  scoreEl.textContent = `${score}/${totalScore}`
+  nextQuestion();
 
 })
 
+function showQuestion() {
+  //Destructuring the object
+  const { correctAnswer, options, question } = quesJSON[currentQuestion];
+
+  //Setting question text content
+  questionElement.textContent = question;
+
+  const shuffledOptions = shuffleOptions(options);
+
+  //Populating the Options div with the buttons
+  shuffledOptions.forEach((opt) => {
+    // Creating button  and append it to the option div
+    const btn = document.createElement("button");
+    btn.textContent = opt;
+    optionEl.appendChild(btn);
+
+    //Event handling on the button
+    btn.addEventListener("click", () => {
+      if (opt === correctAnswer) {
+        score++;
+      } else {
+        score = score - 0.25;
+      }
+      console.log(score);
+      scoreEl.textContent = `Score: ${score}/${totalScore}`;
+      nextQuestion();
+    });
+  });
+}
+
+//Shuffling the options
+function shuffleOptions(options) {
+  for (let i = options.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * i + 1);
+
+    [options[i], options[j]] = [options[j], options[i]];
+  }
+  // console.log(options);
+  return options;
+}
+shuffleOptions([1, 2, 3, 4, 5]);
+
+// Next Question
 function nextQuestion(){
   currentQuestion++;
-
+  optionEl.textContent = '';
+  if(currentQuestion >= quesJSON.length){
+    questionElement.textContent = "Quiz Completed";
+    // optionEl.textContent = "";
+    nextEl.remove();
+  }else{
+    showQuestion();
+  }
 }
-// //Shuffling the options
-// function shuffleOptions(options){
-//   for(let i = options.length-1; i>0; i--){
-//     const j = 
-//   };
-//   [options[3],options[0]] =[options[0], options[3]];
-//   console.log(options);
-// }
-
 
 
